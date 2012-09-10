f69a4ee13f346f2a5b70b6c9858d466f84922bd2
Jon Packer <contact@jonpacker.com>
2012-09-10 11:44:04 +0200

    extract src from olt-responsive

---
+++ jquery.tap.js
@@ -0,0 +1,71 @@
+$.fn.tap =  (function() {
+  var incrementalElementId = 0;
+  var mutex = 0;
+
+  return function(threshold, callback) {
+    if (typeof threshold === 'function') {
+      callback = threshold;
+      threshold = 15;
+    }
+    if ('ontouchstart' in window) {
+      this.each(function() {
+        var moveDistance = 0;
+        var touch = null;
+        var elementId = ++incrementalElementId;
+        var startPoint = null
+        var touching = false;
+        var self = this;
+        var $self = $(this);
+
+        $self.data('hearing-taps', true);
+
+        $self.bind('touchstart', function(e) {
+          if (mutex != 0) return;
+          else mutex = elementId;
+
+          touching = true;
+          moveDistance = 0;
+
+          if (e.originalEvent.touches && e.originalEvent.touches[0]) {
+            touch = e.originalEvent.touches[0];
+            startPoint = { x: touch.screenX, y: touch.screenY}
+          }
+        });
+        $self.bind('touchend', function() {
+          if (mutex == elementId) mutex = 0;
+          if (!touching) return;
+          touching = false;
+          if (moveDistance < threshold) {
+            callback.call(self);
+          } else {
+            $self.trigger('tap-failed');
+          }
+        })
+        $self.bind('touchmove', function(e) {
+          if (!touching) return;
+          if (e.originalEvent.touches.length == 0 || startPoint === null) {
+            return touching = false;
+          }
+
+          touch = e.originalEvent.touches[0];
+          
+          moveDistance = Math.sqrt(Math.pow(touch.screenX - startPoint.x, 2) +
+                                   Math.pow(touch.screenY - startPoint.y, 2));
+
+          if (moveDistance > threshold) {
+            $self.trigger('exceed-tap-threshold');
+            touching = false;
+          }
+        })
+        $self.bind('touchcancel', function() {
+          if (mutex == elementId) mutex = 0;
+          touching = false;
+          $self.trigger('tap-failed');
+        })
+      })
+    } else {
+      this.click(callback);
+    }
+    return this;
+  }
+})()
